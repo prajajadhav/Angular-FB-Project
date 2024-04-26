ff39e6fef2d808c68b527d07e07ea1d36c5dbd73
Prajakta Jadhav <prajujadhav510@gmail.com>
2024-04-26 17:37:29 +0530

    home component updated

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  images: any[] = [
+    'https://images-na.ssl-images-amazon.com/images/I/51DR2KzeGBL._AC_.jpg',
+    'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
+    'https://i.pinimg.com/474x/e0/03/1b/e0031b858ccfcf7c88af8f8ff100d32d.jpg',
+    'https://cdn.pixabay.com/photo/2017/07/18/18/24/dove-2516641_960_720.jpg',
+    'https://c0.wallpaperflare.com/preview/956/761/225/5be97da101a3f.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/9/9a/Swepac_FB_465%2C_RV70%2C_with_passing_lorry.jpg'
+  ];
+
+
+  constructor(){}
+ngOnInit(): void {
+  
+}
+postMessage(form: NgForm): void {}
+  
 }
